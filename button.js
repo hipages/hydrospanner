6bbfafe1d9c7b1bbd42888e1097f37b962983a0a
jaydenharrod <jaydenharrod@HIP01049-Jayden.local>
2018-07-02 17:38:12 +1000

    Add preview button to Opus PRs

--- button.js
+++ button.js
@@ -10,11 +10,25 @@ function slugify(text) {
     .replace(/_/g, '-');
 }
 
-const repos = ['hip-new'];
+const repos = ['hip-new', 'opus'];
+const hipDomain = `www-hip-new`;
+const opusDomain = `opus`;
+function findDomain() {
+  switch (true) {
+    case location.pathname.includes('hip-new'):
+      return hipDomain;
+      break;
+    case location.pathname.includes('opus'):
+      return opusDomain;
+      break;
+    default:
+      return null;
+  }
+}
 
 function renderButtons(parent, branch) {
   const dubdubdub = `
-<a href="https://${branch}-www-hip-new.k8s.hipages.com.au/" class="btn btn-outline btn-sm border-blue">
+<a href="https://${branch}-${findDomain()}.k8s.hipages.com.au/" class="btn btn-outline btn-sm border-blue">
       Preview www
   </a>`;
   const admin = `<a href="https://${branch}-admin-hip-new.k8s.hipages.com.au/" style="margin-left:5px" class="btn btn-outline btn-sm border-blue">
@@ -25,12 +39,12 @@ function renderButtons(parent, branch) {
 }
 
 function findAndRender() {
- // Only run on selected repos;
+  // Only run on selected repos;
   if (!repos.find(repo => location.pathname.indexOf(repo) > -1)) {
     return false;
   }
 
-  // PR view  
+  // PR view
   if (location.pathname.indexOf('pull') > -1) {
     const branch = slugify($('.head-ref span').text());
     console.log(branch);
